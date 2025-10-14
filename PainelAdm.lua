65119710667655191d6e6f035b95207ddc8793bd
MineBloxYT <canaldegtamodsmods@gmail.com>
2025-10-13 21:32:06 -0300

    PainelAdm.lua

--- PainelAdm.lua
+++ PainelAdm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(https://github.com/canaldegtamodsmods-spec/Painel-Adm/blob/main/PainelAdmBrook.lua", true))()
+loadstring(game:HttpGet(https://github.com/canaldegtamodsmods-spec/Painel-Adm/blob/main/PainelAdmBrook", true))()
